76c8486961cc646b8142e183a18dbdd16acb1df7
Keyan Namaghi <keyan.namaghi@sainsburys.co.uk>
2024-04-25 21:37:01 +0100

    🚨

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const BRANCH_ID = 'BRANCH_ID'
+const PR_ID = 'PR_ID'
+
 const getTicketId = (url) => {
   if (url.includes('selectedIssue=')) {
     return url.split('selectedIssue=')[1]
@@ -8,16 +11,31 @@ const getTicketId = (url) => {
 chrome.runtime.onInstalled.addListener(function () {
   chrome.contextMenus.create({
     title: '🦥 Branch Name',
-    id: 'branch-name',
+    id: BRANCH_ID,
     contexts: ['all'],
     documentUrlPatterns: ['https://sainsburys-tech.atlassian.net/*']
   })
 })
 
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.contextMenus.create({
+    title: '🦥 PR Name',
+    id: PR_ID,
+    contexts: ['all'],
+    documentUrlPatterns: ['https://github.com/*']
+  })
+})
+
 chrome.contextMenus.onClicked.addListener(({ menuItemId }) => {
-  if (menuItemId === 'branch-name') {
+  if (menuItemId === BRANCH_ID) {
     chrome.tabs.query({ active: true, currentWindow: true }, async function (tab) {
       await chrome.tabs.sendMessage(tab[0].id, { action: 'createBranchName', id: getTicketId(tab[0].url) })
+    })
+  }
+
+  if (menuItemId === PR_ID) {
+    chrome.tabs.query({ active: true, currentWindow: true }, async function (tab) {
+      await chrome.tabs.sendMessage(tab[0].id, { action: 'createPRName' })
     })
   }
 })
